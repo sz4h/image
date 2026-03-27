2453c49270cd75e873723aaf6d95c7cd8deb5da2
Ahmed Safaa <Mello21century@users.noreply.github.com>
2026-03-27 07:35:30 +0300

    Refactor ImageController for nullable types and error handling

--- src/Http/Controllers/ImageController.php
+++ src/Http/Controllers/ImageController.php
@@ -4,34 +4,28 @@ namespace Space\Image\Http\Controllers;
 
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Str;
 use Intervention\Image\Exception\NotFoundException;
+use Intervention\Image\Exceptions\DecoderException;
 use Intervention\Image\Interfaces\ImageInterface;
 use Intervention\Image\Laravel\Facades\Image;
 use Masterminds\HTML5\Exception;
 use Space\Image\Exceptions\CanNotHandleNonImageType;
-use Intervention\Image\Exceptions\DecoderException;
 
 class ImageController extends Controller
 {
-    private null|string $url;
+    private ?string $url;
 
     private Collection $params;
 
-    public function resize(null|string $url = null)
+    public function resize(?string $url = null)
     {
         $this->url = $url;
         $this->setParams();
 
-
         // Make the image
-        if (!is_null($url)) {
-            try {
-                $image = Image::read($this->path);
-            } catch (NotFoundException|Exception|DecoderException $e) {
-                $image = Image::read(config('sz4h-image.not_found_image_path'));
-            }
-        } else {
+        try {
+            $image = $this->getImage($url);
+        } catch (NotFoundException|Exception|DecoderException $e) {
             $image = Image::read(config('sz4h-image.not_found_image_path'));
         }
 
@@ -80,7 +74,7 @@ class ImageController extends Controller
         }
 
         /* Add the remaining path as an url */
-        if (!str_contains(request()->getQueryString(), 'url=')) {
+        if (! str_contains(request()->getQueryString(), 'url=')) {
             $this->params->put('url', $pathParts->join('/'));
         }
 
@@ -88,26 +82,25 @@ class ImageController extends Controller
         $this->params = $this->params->merge($preset);
         $this->params->put('type', $type);
 
-
         /* Fall down to default params if not set by request */
         $this->defaultParams();
 
-        if (!str((string)$this->params->get('url'))->lower()->endsWith([
-            '.jpg', '.gif', '.bmp', '.jpeg', '.bmp', '.webp', '.png'
+        if (! str((string) $this->params->get('url'))->lower()->endsWith([
+            '.jpg', '.gif', '.bmp', '.jpeg', '.bmp', '.webp', '.png',
         ])) {
             throw new CanNotHandleNonImageType(last(explode('.', $this->params->get('url'))));
-        };
+        }
 
         // Width
         $this->w = $this->params->get('w');
         // Height
         $this->h = $this->params->get('h');
         // Ratio
-        $this->r = (bool)$this->params->get('retain');
+        $this->r = (bool) $this->params->get('retain');
         // Watermark
-        $this->wm = (bool)$this->params->get('watermark');
+        $this->wm = (bool) $this->params->get('watermark');
         // Crop
-        $this->c = (bool)$this->params->get('crop');
+        $this->c = (bool) $this->params->get('crop');
         // Background
         $this->bg = empty($this->params->get('bg')) ? null : $this->params->get('bg');
 
@@ -115,45 +108,49 @@ class ImageController extends Controller
 
     }
 
-    /**
-     * @param mixed $selectedPreset
-     * @return Collection
-     */
     protected function getPreset(mixed $selectedPreset): Collection
     {
         return collect(explode('&', $selectedPreset))
-            ->filter(fn($item) => str_contains($item, '='))
-            ->map(fn($item) => explode('=', $item))
+            ->filter(fn ($item) => str_contains($item, '='))
+            ->map(fn ($item) => explode('=', $item))
             ->keyBy('0')
-            ->map(fn($item) => @$item[1])
-            ->map(fn($item) => urldecode($item))
-            ->map(fn($item) => ($item === 'false' || $item == 0) ? false : $item);
+            ->map(fn ($item) => @$item[1])
+            ->map(fn ($item) => urldecode($item))
+            ->map(fn ($item) => ($item === 'false' || $item == 0) ? false : $item);
     }
 
+    protected function getImage(?string $url = null)
+    {
+        if (is_null($url) && ! isset($this->path)) {
+            return Image::read(config('sz4h-image.not_found_image_path'));
+        }
+
+        return Image::read($this->path);
+    }
+    
     protected function defaultParams()
     {
-        if (!$this->params->has('w')) {
+        if (! $this->params->has('w')) {
             $this->params->put('w', config('sz4h-image.default.w', null));
         }
-        if (!$this->params->has('h')) {
+        if (! $this->params->has('h')) {
             $this->params->put('h', config('sz4h-image.default.h', null));
         }
-        if (!$this->params->has('crop')) {
+        if (! $this->params->has('crop')) {
             $this->params->put('crop', config('sz4h-image.default.crop', true));
         }
-        if (!$this->params->has('retain')) {
+        if (! $this->params->has('retain')) {
             $this->params->put('retain', config('sz4h-image.default.retain', true));
         }
-        if (!$this->params->has('bg')) {
+        if (! $this->params->has('bg')) {
             $this->params->put('bg', config('sz4h-image.default.bg'));
         }
-        if (!$this->params->has('watermark')) {
+        if (! $this->params->has('watermark')) {
             $this->params->put('watermark', config('sz4h-image.default.watermark', false));
         }
     }
 
     /**
-     * @param ImageInterface $image
      * @return void
      */
     protected function watermark(ImageInterface $image): ImageInterface
@@ -177,6 +174,7 @@ class ImageController extends Controller
             config('sz4h-image.watermark.offsetX'),
             config('sz4h-image.watermark.offsetY')
         );
+
         return $image;
     }
 }
